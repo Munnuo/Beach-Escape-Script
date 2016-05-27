927aaa79cbe82b384b8ad52d9550c3361d146c36
Munnuo <charles.warr@mvsu.edu>
2016-05-27 03:02:56 -0500

    Update ThirdPersonCamera.cs

--- ThirdPersonCamera.cs
+++ ThirdPersonCamera.cs
@@ -3,19 +3,19 @@ using System.Collections;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
-    public Transform target;
-    public GameObject player;
-    public float speed = 0.1f;
+    public Transform target;	// public reference to camera view object transform that the editor selects
+    public GameObject player;	// public reference to any game object that editor selects inside unity editor
+    public float speed = 0.1f;	// float that defines the speed of the camera movement.
 
-    private float distance = -2f;
-    private float height = 0f;
-    private float cHeight;
-    private Transform userTransform;
-    private Animator anim;
-    private HashIDs hash;
-    private bool camXNeg;
-    private bool camXPos;
-    private Vector3 offsetX;
+    private float distance = -2f;	// distance between the camera and the targeted player (Game Object)
+    private float height = 0f;		// height of the camera in the Y axis relative to the player
+    private float cHeight;		// Change in height of the camera in the Y axis
+    private Transform userTransform;	// the transform of input received from the user
+    private Animator anim;		// The reference to the animator component 
+    private HashIDs hash;		// The reference to the hash IDs used to label specific objects in-game
+    private bool camXNeg;		// The negative boolean for camera movement
+    private bool camXPos;		// The positive boolean for camera movement
+    private Vector3 offsetX;		
     private float angX;
    
 
@@ -24,18 +24,18 @@ public class ThirdPersonCamera : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-        if(target == null)
+        if(target == null)	// Check to see if a camera view object is selected
         {
             Debug.LogWarning("No target selected");
         }
 
-        if(player == null)
+        if(player == null)	// check to see if a player object is selected
         {
             Debug.LogWarning("A player is needed");
         }
 
 
-        userTransform = transform;
+        userTransform = transform;	// The transform of the camera
 
         anim = player.GetComponent<Animator>();
         hash = GameObject.FindGameObjectWithTag(tags.gameController).GetComponent<HashIDs>();
@@ -109,12 +109,13 @@ public class ThirdPersonCamera : MonoBehaviour
     void LateUpdate()
     {
         
-        offsetX = Quaternion.AngleAxis(angX * speed*.2f, Vector3.up) * offsetX;
+        offsetX = Quaternion.AngleAxis(angX * speed*.2f, Vector3.up) * offsetX; // This value keeps the camera at a constant distance
+        									// away from the player
 
-        if(!anim.GetBool(hash.deathBool))
-        userTransform.position = target.position + offsetX;
+        if(!anim.GetBool(hash.deathBool)) // if the player dies, the camera stops following them.
+        userTransform.position = target.position + offsetX; // This line changes the position of the camera to follow the player
       
-        userTransform.LookAt(target);
+        userTransform.LookAt(target); // this changes the camera view to look at the targeted camera view
 
     }
 }
